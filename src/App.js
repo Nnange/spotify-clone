836cca17bc38ec74b89b0c9db49dae670d1245ac
Awong Nnange <awong.nnange@yahoo.com>
2020-10-22 17:37:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const id = 756709098e964f248bc984e064603b17
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>heyy app</h1>
     </div>
   );
 }
