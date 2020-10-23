a60f5ae528192d3fe57d0736b019b18cd0eb98f3
Awong Nnange <awong.nnange@yahoo.com>
2020-10-23 22:15:24 +0200

    Updates

---
+++ precache-manifest.14ba0ac804d618776cb3dec5ab4867a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185c2fa011dc81a0b27c26a006990641",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "4caaeff53c1952d3fa76",
+    "url": "/spotify-clone/static/css/main.e30f6fdf.chunk.css"
+  },
+  {
+    "revision": "27b011b6cf9a1f07d908",
+    "url": "/spotify-clone/static/js/2.e12679f6.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/spotify-clone/static/js/2.e12679f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4caaeff53c1952d3fa76",
+    "url": "/spotify-clone/static/js/main.36b2dd52.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
